24b83b53b83471eb4b8b0778440fcc3b50a8ef61
kavya-2501 <kavyaa.250501@gmail.com>
2024-06-26 15:03:02 +0530

    fix:added argument for validatorfunction in routes

--- src/routes/v1/createSubmissionRoute.ts
+++ src/routes/v1/createSubmissionRoute.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import createSubmission from '../../controllers/createSubmissionController';
+import { createSubmissionZodSchema } from '../../dtos/createSubmissionDto';
 import { validateCreateSubmissionDto } from '../../validators/createSubmissionValidator';
 
 const problemSubmission=express.Router();
 
-problemSubmission.post('/',validateCreateSubmissionDto,createSubmission);
+problemSubmission.post('/',validateCreateSubmissionDto(createSubmissionZodSchema),createSubmission);
 
 export default problemSubmission;
